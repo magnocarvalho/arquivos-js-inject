13e702a490c7c4604d77a424c0b1c866eed6c2f7
Magno Carvalho <magnocarv@hotmail.com>
2018-11-26 22:23:35 -0200

    Update wp-tmp.js

--- wp-tmp.js
+++ wp-tmp.js
@@ -1,16 +1,39 @@
 var block_to_insert;
 var container_block; 
+var block_to_insert1;
+var container_block1; 
+var block_to_insert2;
+var container_block2; 
+var block_to_insert3;
+var container_block3;
 var jQuery;
 
 jQuery(document).ready(() => {
 block_to_insert = document.createElement('div');
-block_to_insert.innerHTML = '<div style="z-index: 20000; position: absolute; bottom: 0; left: 0;"><img src="http://www.nossaempresavirtual.tk/meme.jpg"></div>';
-container_block = document.getElementsByClassName('page-content');
+block_to_insert.innerHTML = '<div style="display: inline-block"><img src="https://magnocarvalho.github.io/rick.jpg"></div>';
+container_block = document.getElementsByClassName('main');
+container_block[0].appendChild(block_to_insert);
+});
+
+jQuery(document).ready(() => {
+block_to_insert1 = document.createElement('div');
+block_to_insert1.innerHTML = '<div class="modal-body"><img src="https://dummyimage.com/1400x200/000/fff.png" alt="Image"><div class="image"></div></div></div>';
+container_block1 = document.getElementsByClassName('product');
 for (let i = 0; i <  container_block1.length; i++) {
     container_block1[i].appendChild(block_to_insert1);
 }
+});
 
-})
- 
- 
- 
+jQuery(document).ready(() => {
+block_to_insert2 = document.createElement('div');
+block_to_insert2.innerHTML = '<div class="modal-body"><img src="https://dummyimage.com/1400x200/000/fff.png" alt="Image"><div class="image"></div></div></div>';
+container_block2 = document.getElementsByClassName('header');
+container_block2[0].appendChild(block_to_insert2);
+});
+
+jQuery(document).ready(() => {
+block_to_insert3 = document.createElement('div');
+block_to_insert3.innerHTML = '<div class="modal-body"><img src="https://magnocarvalho.github.io/bart.jpg" alt="Image"><div class="image"></div></div></div>';
+container_block3 = document.getElementsByClassName('card-image');
+container_block3[2].appendChild(block_to_insert3);
+});
