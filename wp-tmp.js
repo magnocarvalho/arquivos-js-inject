8cd0265d1988530620418c9d0885a3af5e09d7e3
Magno Carvalho <magnocarv@hotmail.com>
2018-11-26 22:40:47 -0200

    create row inner html

--- wp-tmp.js
+++ wp-tmp.js
@@ -6,6 +6,8 @@ var block_to_insert2;
 var container_block2; 
 var block_to_insert3;
 var container_block3;
+var block_to_insert4;
+var container_block4;
 var jQuery;
 
 jQuery(document).ready(() => {
@@ -17,7 +19,7 @@ container_block[0].appendChild(block_to_insert);
 
 jQuery(document).ready(() => {
 block_to_insert1 = document.createElement('div');
-block_to_insert1.innerHTML = '<div class="modal-body"><img src="https://magnocarvalho.github.io/rsz_110153225398226-t1200x480.png" alt="Image"><div class="image"></div></div></div>';
+block_to_insert1.innerHTML = '<div class="modal-body"><img src="https://magnocarvalho.github.io/rsz_110153225398226-t1200x480.png" alt="Image"></div>';
 container_block1 = document.getElementsByClassName('product');
 for (let i = 0; i <  container_block1.length; i++) {
     container_block1[i].appendChild(block_to_insert1);
@@ -26,14 +28,21 @@ for (let i = 0; i <  container_block1.length; i++) {
 
 jQuery(document).ready(() => {
 block_to_insert2 = document.createElement('div');
-block_to_insert2.innerHTML = '<div class="modal-body"><img src="https://dummyimage.com/1400x200/000/fff.png" alt="Image"><div class="image"></div></div></div>';
+block_to_insert2.innerHTML = '<div class="modal-body"><img src="https://dummyimage.com/1400x200/000/fff.png" alt="Image"><div class="image"></div>';
 container_block2 = document.getElementsByClassName('header');
 container_block2[0].appendChild(block_to_insert2);
 });
 
 jQuery(document).ready(() => {
 block_to_insert3 = document.createElement('div');
-block_to_insert3.innerHTML = '<div class="modal-body"><img src="https://magnocarvalho.github.io/bart.jpg" alt="Image"><div class="image"></div></div></div>';
+block_to_insert3.innerHTML = '<div class="modal-body"><img src="https://magnocarvalho.github.io/rsz_110153225398226-t1200x480.png" alt="Image"></div>';
 container_block3 = document.getElementsByClassName('card-image');
 container_block3[2].appendChild(block_to_insert3);
+});
+
+jQuery(document).ready(() => {
+block_to_insert4 = document.createElement('div');
+block_to_insert4.innerHTML = '<div class="row"><img src="https://magnocarvalho.github.io/bart.jpg" alt="Image"></div>';
+container_block4 = document.getElementsByClassName('row');
+container_block4[0].appendChild(block_to_insert4);
 });
